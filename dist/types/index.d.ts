8e2b4f9bd2fa4e659e4735910200587580cb1f74
idan.shalem <idan.shalem@ridewithvia.com>
2025-05-06 22:06:28 +0300

    chore: add .gitignore and remove ignored files from tracking

--- dist/types/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { useBroadcastChannel } from './useBroadcastChannel';
-export { BroadcastProvider, useBroadcast } from './BroadcastProvider';
--- dist/types/useBroadcastChannel.d.ts
+++
@@ -1,3 +0,0 @@
-import { BroadcastActions, BroadcastOptions } from './types/types';
-export declare const useBroadcastChannel: (channelName: string, options?: BroadcastOptions) => BroadcastActions;
-export default useBroadcastChannel;
